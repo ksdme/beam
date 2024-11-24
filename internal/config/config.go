3d7ef9a6c1ff88a166278334a8861fc12e9faf11
Kilari Teja <kilariteja9@gmail.com>
2024-11-24 23:08:40 +0530

    Add Secret configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,11 +6,17 @@ import (
 )
 
 type Config struct {
+	Secret      string
 	Addr        string
 	HostKeyFile string
 }
 
 func LoadConfigFromEnv() (*Config, error) {
+	secret := os.Getenv("BEAM_SECRET")
+	if secret == "" {
+		return nil, fmt.Errorf("BEAM_SECRET missing")
+	}
+
 	addr := os.Getenv("BEAM_ADDR")
 	if addr == "" {
 		addr = ":2222"
@@ -18,10 +24,11 @@ func LoadConfigFromEnv() (*Config, error) {
 
 	key := os.Getenv("BEAM_HOST_KEY_FILE")
 	if key == "" {
-		return nil, fmt.Errorf("BEAM_HOST_KEY_FILE unavailable")
+		return nil, fmt.Errorf("BEAM_HOST_KEY_FILE missing")
 	}
 
 	return &Config{
+		Secret:      secret,
 		Addr:        addr,
 		HostKeyFile: key,
 	}, nil
